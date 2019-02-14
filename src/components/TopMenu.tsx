db8378df6732fb21ca9f9a63579ac429f8dad029
au508062 <201508592@post.au.dk>
2019-02-14 11:45:08 +0100

    init commit

---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import "../component_css/TopMenu.css"
+
+type Props = {
+    title: string;
+}
+
+export class TopMenuItem extends Component<Props> {
+    render() {
+        const title = this.props.title;
+        return (
+            <li className="top-menu-item">
+                <a>
+                    {title}
+                </a>
+            </li>
+        )
+    }
+}
+
+export class TopMenu extends Component {
+    render () {
+        return (
+            <ul className="top-menu">
+                <TopMenuItem title="Events" />
+                <TopMenuItem title="Prices" />
+                <TopMenuItem title="Facebook" />
+            </ul>
+        )
+    }
+}
+
+
+
+
+
